78b405a2f39f039226b8a28e7675e40a1c4ede42
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-18 19:42:01 -0600

    Functional programming - Anonymous Inner Class

--- FP-00-LambdaSyntax/src/main/java/anonymousinnerclass/LambdaIntroRunner.java
+++ FP-00-LambdaSyntax/src/main/java/anonymousinnerclass/LambdaIntroRunner.java
@@ -3,7 +3,11 @@ package anonymousinnerclass;
 public class LambdaIntroRunner {
     public static void main(String[] args) {
 
+        Moveable moveXMile = new DistantMover();
+        moveXMile.move(15);
 
+        Moveable moveShort = new ShortDistanceMover();
+        moveShort.move(5);
 
     }
 
--- FP-00-LambdaSyntax/src/main/java/anonymousinnerclass/Moveable.java
+++ FP-00-LambdaSyntax/src/main/java/anonymousinnerclass/Moveable.java
@@ -1,4 +1,23 @@
 package anonymousinnerclass;
 
 public interface Moveable {
+
+    void move(int mile);
+
+}
+
+class DistantMover implements Moveable{
+
+    public void move(int mile) {
+        System.out.println("moving " + mile);
+    }
+
+}
+
+class ShortDistanceMover implements Moveable{
+
+    public void move(int mile) {
+        System.out.println("moving short mile " + mile);
+    }
+
 }
