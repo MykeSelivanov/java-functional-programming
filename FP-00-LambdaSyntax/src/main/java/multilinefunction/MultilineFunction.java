1d1a67db99c7d1e2e4cb92e4a3ef3ebafad80aa8
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-20 14:00:00 +0100

    Multiline Function

--- FP-00-LambdaSyntax/src/main/java/multilinefunction/MultilineFunction.java
+++ FP-00-LambdaSyntax/src/main/java/multilinefunction/MultilineFunction.java
@@ -3,6 +3,8 @@ package multilinefunction;
 public class MultilineFunction {
     public static void main(String[] args) {
 
+        // Need to wrap the body with { }
+
         LengthOfString len = str -> {
             int l = str.length();
             System.out.println("The length is " + l);
