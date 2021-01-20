6de8e18e0d02b65f16d1b7ddea4cfd8f0c0b3f8d
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-20 14:00:00 +0100

    Parameters with Return type

--- FP-00-LambdaSyntax/src/main/java/parameterswithreturntype/ParametersWithReturn.java
+++ FP-00-LambdaSyntax/src/main/java/parameterswithreturntype/ParametersWithReturn.java
@@ -3,6 +3,8 @@ package parameterswithreturntype;
 public class ParametersWithReturn {
     public static void main(String[] args) {
 
+        // You don't need () if there is only one parameter
+
         LengthOfString len = str -> str.length();
         int length = len.length("Hello World");
 
