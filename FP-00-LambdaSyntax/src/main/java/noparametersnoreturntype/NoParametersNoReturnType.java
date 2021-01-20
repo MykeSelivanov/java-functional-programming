424b515dddd6997e84a682ac9ef5ed45a03db9e0
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-20 14:00:00 +0100

    Parameters and Return Type

--- FP-00-LambdaSyntax/src/main/java/noparametersnoreturntype/NoParametersNoReturnType.java
+++ FP-00-LambdaSyntax/src/main/java/noparametersnoreturntype/NoParametersNoReturnType.java
@@ -3,6 +3,8 @@ package noparametersnoreturntype;
 public class NoParametersNoReturnType {
     public static void main(String[] args) {
 
+        // Simple one line with empty ()
+
         Name name = () -> System.out.println("John");
         name.myName();
 
--- FP-00-LambdaSyntax/src/main/java/parametersnoreturntype/ParametersNoReturnType.java
+++ FP-00-LambdaSyntax/src/main/java/parametersnoreturntype/ParametersNoReturnType.java
@@ -3,6 +3,8 @@ package parametersnoreturntype;
 public class ParametersNoReturnType {
     public static void main(String[] args) {
 
+        // Parameters in ()
+
         MathOperation add = (a, b) -> System.out.println(a + b);
         add.operation(10,20);
 
