f1c573dff7ef074b2e93805ba231e23adcf2acc3
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-21 22:21:32 -0600

    Lambda Syntax

---
+++ FP-00-LambdaSyntax/src/main/java/lambdasyntax/LambdaIntroRunner.java
@@ -0,0 +1,9 @@
+package lambdasyntax;
+
+public class LambdaIntroRunner {
+    public static void main(String[] args) {
+
+        Measurable measurable1 = () -> System.out.println("measuring square");
+
+    }
+}
---
+++ FP-00-LambdaSyntax/src/main/java/lambdasyntax/Moveable.java
@@ -0,0 +1,11 @@
+package lambdasyntax;
+
+@FunctionalInterface
+public interface Moveable {
+    void move(int mile);
+}
+
+@FunctionalInterface
+interface Measurable{
+    void measure();
+}
