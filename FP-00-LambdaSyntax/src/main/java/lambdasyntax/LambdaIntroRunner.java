2d4964a9d7bccf1cec23d0603d91177af7994f83
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-21 22:23:10 -0600

    Lambda Syntax

--- FP-00-LambdaSyntax/src/main/java/lambdasyntax/LambdaIntroRunner.java
+++ FP-00-LambdaSyntax/src/main/java/lambdasyntax/LambdaIntroRunner.java
@@ -4,6 +4,16 @@ public class LambdaIntroRunner {
     public static void main(String[] args) {
 
         Measurable measurable1 = () -> System.out.println("measuring square");
+        Measurable measurable2 = () -> System.out.println("measuring rectangle");
+
+        Measurable measurable3 = () -> {
+            System.out.println("measuring circle - step 1");
+            System.out.println("measuring circle - step 2");
+            System.out.println("measuring circle - step 3");};
+
+        measurable1.measure();
+        measurable2.measure();
+        measurable3.measure();
 
     }
 }
