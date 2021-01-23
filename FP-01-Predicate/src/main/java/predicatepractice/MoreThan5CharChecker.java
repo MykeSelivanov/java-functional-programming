b84a62e850ad7a7b49a82b758045a02164d14af1
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-22 21:50:53 -0600

    Built in FI - Predicate

--- FP-01-Predicate/src/main/java/predicatepractice/MoreThan5CharChecker.java
+++ FP-01-Predicate/src/main/java/predicatepractice/MoreThan5CharChecker.java
@@ -1,4 +1,9 @@
 package predicatepractice;
 
-public class MoreThan5CharChecker {
+import java.util.function.Predicate;
+
+public class MoreThan5CharChecker implements Predicate<String> {
+    public boolean test(String s) {
+        return s.length() > 5;
+    }
 }
