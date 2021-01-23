08fbf889956cf69d76be9d7248fa1e3ef0eae996
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-22 21:51:29 -0600

    Built in FI - Predicate

--- FP-01-Predicate/src/main/java/predicatepractice/PredicatePracticeRunner.java
+++ FP-01-Predicate/src/main/java/predicatepractice/PredicatePracticeRunner.java
@@ -1,9 +1,11 @@
 package predicatepractice;
 
+import java.util.function.Predicate;
+
 public class PredicatePracticeRunner {
     public static void main(String[] args) {
 
-
+        Predicate<String> moreThan5Char;
 
     }
 
