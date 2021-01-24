b28f155ed1764f7e83d2d836d72cc3c8082c96db
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-24 11:58:09 -0600

    Built in FI - Predicate

--- FP-01-Predicate/src/main/java/predicatepractice/PredicatePracticeRunner.java
+++ FP-01-Predicate/src/main/java/predicatepractice/PredicatePracticeRunner.java
@@ -1,11 +1,17 @@
 package predicatepractice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicatePracticeRunner {
     public static void main(String[] args) {
 
         Predicate<String> moreThan5Char;
+
+        List<String> names = Arrays.asList("James","John","Pete","Mike","Hanna","Diana");
+
 
     }
 
