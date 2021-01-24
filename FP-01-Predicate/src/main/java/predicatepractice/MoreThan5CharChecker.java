fb5bf90a65e8ffd11f495e53938a018227b4f12f
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-24 17:53:46 -0600

    Built in FI - Predicate

--- FP-01-Predicate/src/main/java/predicatepractice/MoreThan5CharChecker.java
+++ FP-01-Predicate/src/main/java/predicatepractice/MoreThan5CharChecker.java
@@ -3,6 +3,8 @@ package predicatepractice;
 import java.util.function.Predicate;
 
 public class MoreThan5CharChecker implements Predicate<String> {
+
+    // Predicate - boolean-valued function of one argument
     public boolean test(String s) {
         return s.length() > 5;
     }
--- FP-01-Predicate/src/main/java/predicatepractice/PredicatePracticeRunner.java
+++ FP-01-Predicate/src/main/java/predicatepractice/PredicatePracticeRunner.java
@@ -8,12 +8,30 @@ import java.util.function.Predicate;
 public class PredicatePracticeRunner {
     public static void main(String[] args) {
 
+        // One way with implementing Predicate in separate MoreThan5CharChecker class which is redundancy
         Predicate<String> moreThan5Char = new MoreThan5CharChecker();
 
-        List<String> names = Arrays.asList("Jameson","Joseph","Pete","Mike","Hanna","Diana");
+        List<String> names = new ArrayList<>();
+        names.add("Joseph");
+        names.add("Peter");
+        names.add("Mike");
+        names.add("Juliet");
 
         names.removeIf(moreThan5Char);
         System.out.println(names);
+
+        // More efficient way without separate class
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(11);
+        numbers.add(15);
+        numbers.add(21);
+        numbers.add(27);
+
+        numbers.removeIf(number -> number > 15);
+        System.out.println(numbers);
+
+
 
 
     }
