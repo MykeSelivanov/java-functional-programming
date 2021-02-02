28cf8a92d016b63f2b9ccda832815a55124711cf
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-01 21:19:31 -0600

    Built in FI - Supplier

---
+++ FP-01-Built-In-FI-Supplier/src/main/java/supplierpractice/SupplierPracticeRunner.java
@@ -0,0 +1,18 @@
+package supplierpractice;
+
+public class SupplierPracticeRunner {
+    public static void main(String[] args) {
+
+        /**
+         * Supplier - Does not accept any arguments and returns an object
+         * Supplier<Double> randomValue = () -> Math.random();
+         * System.out.println(randomValue.get());
+         */
+
+
+
+
+    }
+
+
+}
