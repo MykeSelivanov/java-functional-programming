e2780ee3bb2aef3fda10012c0c1bd1dc840dc7cb
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-02 21:49:03 -0600

    Built in FI - Comparator with Function

--- FP-01-Comparator-Function/src/main/java/comparatorwithfunction/ComparatorWithFunctionRunner.java
+++ FP-01-Comparator-Function/src/main/java/comparatorwithfunction/ComparatorWithFunctionRunner.java
@@ -7,20 +7,21 @@ import java.util.List;
 public class ComparatorWithFunctionRunner {
     public static void main(String[] args) {
 
-        Comparator<String> byCharComparison =
+//        Comparator<String> byCharComparison =
                 // old school way
                 // ((o1, o2) -> o1.substring(o1.length()-1).compareTo(o2.substring(o2.length()-1)));
 
                 // shorter syntax with Function
-                Comparator.comparing(str -> str.substring(str.length()-1));
+                // Comparator.comparing(str -> str.substring(str.length()-1));
 
-        List<String> names = Arrays.asList("John","Peter","Hannah","Gregory","Amanda","Nadia");
+        List<String> names = Arrays.asList("Johna","Peter","Hannah","Gregory","Amanda","Nadia");
 
-        names.sort(byCharComparison);
+        // call directly in sort
+        names.sort(Comparator.comparing(str -> str.substring(str.length()-1)));
 
         int i = 1;
         for (String name: names) {
-            System.out.println("sorted at " + i + ": " + name);
+            System.out.println("sorted by last char at " + i + ": " + name);
             i++;
         }
 
