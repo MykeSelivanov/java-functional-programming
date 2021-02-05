3c0a68d32f5411ba302062c61df595f9cb773c46
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-04 23:35:34 -0600

    Built in FI - UnaryOperator

---
+++ FP-01-Built-In-FI-UnaryOperator/src/main/java/unaryoperator/UnaryOperatorPractice.java
@@ -0,0 +1,27 @@
+package unaryoperator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorPractice {
+    public static void main(String[] args) {
+
+        /**
+         * UnaryOperator<T> extends Function<T,T>
+         * accepts one type and returns the same type
+         */
+
+        List<Integer> nums = List.of(1,2,3,4,5,6,7,8);
+
+        UnaryOperator<Integer> operator = i -> i * 100;
+
+        List<Integer> newList = mapper(list, operator);
+
+    }
+
+    private static <T> List<T> mapper(List<T> list, UnaryOperator<T> operator){
+        List<T> list1 = new ArrayList<>();
+    }
+
+}
