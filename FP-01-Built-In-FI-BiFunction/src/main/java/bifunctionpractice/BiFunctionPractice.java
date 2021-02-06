66275eb8ea2eb5bf286faf17414dd6af377fca36
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-05 22:33:29 -0600

    Built in FI - BiFunction

--- FP-01-Built-In-FI-BiFunction/src/main/java/bifunctionpractice/BiFunctionPractice.java
+++ FP-01-Built-In-FI-BiFunction/src/main/java/bifunctionpractice/BiFunctionPractice.java
@@ -1,12 +1,20 @@
 package bifunctionpractice;
 
+import java.util.function.BiFunction;
+
 public class BiFunctionPractice {
     public static void main(String[] args) {
 
         /**
-         * BiFunction takes 2 arguments and 1 return type
-         *
+         * BiFunction takes 2 arguments and returns 1 value
+         * arguments and return can be same as well as different type
          */
+
+        BiFunction<String,String,String> biFunction = (str, str1) -> str + str1;
+        System.out.println(biFunction.apply("Hello","World"));
+
+        BiFunction<Character, String, Integer> biFunction1 = (character, s) -> (character + s).length();
+        System.out.println(biFunction1.apply('H',"ello"));
 
     }
 
