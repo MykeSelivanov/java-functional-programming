0550ee21beee2b2ab7a97e56f5a77156142cb1b3
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-05 22:27:49 -0600

    Built in FI - UnaryOperator

---
+++ FP-01-Built-In-FI-BiFunction/src/main/java/bifunctionpractice/BiFunctionPractice.java
@@ -0,0 +1,13 @@
+package bifunctionpractice;
+
+public class BiFunctionPractice {
+    public static void main(String[] args) {
+
+        /**
+         * BiFunction takes 2 arguments and 1 return type
+         *
+         */
+
+    }
+
+}
