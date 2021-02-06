3954c0bff9293c6e39aeafdb0d32958d570bb7ba
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-05 22:42:13 -0600

    Built in FI - BinaryOperator

---
+++ FP-01-Built-In-FI-BinaryOperator/src/main/java/binaryoperatorpractice/BinaryOperatorPractice.java
@@ -0,0 +1,14 @@
+package binaryoperatorpractice;
+
+public class BinaryOperatorPractice {
+    public static void main(String[] args) {
+
+        /**
+         * BinaryOperator extends from BiFunction
+         * and restricts arguments and return to be of the same type
+         */
+
+
+    }
+
+}
