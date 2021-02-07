87342348826031cc730988ae217401fe54ce55a9
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-06 20:27:36 -0600

    Built in FI - BinaryOperator

--- FP-01-Built-In-FI-BinaryOperator/src/main/java/binaryoperatorpractice/BinaryOperatorPractice.java
+++ FP-01-Built-In-FI-BinaryOperator/src/main/java/binaryoperatorpractice/BinaryOperatorPractice.java
@@ -1,5 +1,7 @@
 package binaryoperatorpractice;
 
+import java.util.function.BinaryOperator;
+
 public class BinaryOperatorPractice {
     public static void main(String[] args) {
 
@@ -8,6 +10,9 @@ public class BinaryOperatorPractice {
          * and restricts arguments and return to be of the same type
          */
 
+        BinaryOperator<String> operator = (a,b) -> a + "@" + b;
+        String str = operator.apply("Mike","gmail.com");
+        System.out.println(str);
 
     }
 
