f51404d548afa5d5c93bb7570c009f63d44fe9a1
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-07 16:35:36 -0600

    Method reference

---
+++ FP-02-Method-Reference/src/main/java/methodreferencepractice/A_Class.java
@@ -0,0 +1,9 @@
+package methodreferencepractice;
+
+public class A_Class {
+
+        static String staticMethod(String a, String b){
+            return a + b;
+        }
+
+}
--- FP-02-Method-Reference/src/main/java/methodreferencepractice/MethodReferencePractice.java
+++ FP-02-Method-Reference/src/main/java/methodreferencepractice/MethodReferencePractice.java
@@ -1,5 +1,6 @@
 package methodreferencepractice;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -8,24 +9,40 @@ public class MethodReferencePractice {
 
         /**
          * 4 types of method references
+         *
          * 1 - Object :: instanceMethod
+         * Method reference to an instance method of an existing object
+         *
          * 2 - Class :: staticMethod
+         * Method reference to a static method of a class
+         *
          * 3 - Class :: instanceMethod
+         *
+         *
          * 4 - Class :: new
          */
 
 
-
+        // Object :: instanceMethod
         // Lambda way
         Consumer<Integer> consumer = element -> System.out.println(element);
         // Method reference way
         Consumer<Integer> consumer1 = System.out :: println;
 
+        // Class :: staticMethod
         // Lambda way
         Supplier<Double> supplier = () -> Math.random();
         // Method reference way
         Supplier<Double> supplier1 = Math :: random;
 
+        // Lambda way
+        BiFunction<String, String, String> biFunction = (a,b) -> A_Class.staticMethod(a, b);
+        // Method reference way
+        BiFunction<String, String, String> biFunction1 = A_Class :: staticMethod;
+
+
     }
 
 }
+
+
