f456b731ac1f3aaa1d7de8cbeba5e7c44f22f166
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-07 16:21:19 -0600

    Method reference

--- FP-02-Method-Reference/src/main/java/methodreferencepractice/MethodReferencePractice.java
+++ FP-02-Method-Reference/src/main/java/methodreferencepractice/MethodReferencePractice.java
@@ -1,9 +1,20 @@
 package methodreferencepractice;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class MethodReferencePractice {
     public static void main(String[] args) {
+
+        /**
+         * 4 types of method references
+         * 1 - Object :: instanceMethod
+         * 2 - Class :: staticMethod
+         * 3 - Class :: instanceMethod
+         * 4 - Class :: new
+         */
+
+
 
         // Lambda way
         Consumer<Integer> consumer = element -> System.out.println(element);
@@ -11,6 +22,9 @@ public class MethodReferencePractice {
         Consumer<Integer> consumer1 = System.out :: println;
 
         // Lambda way
+        Supplier<Double> supplier = () -> Math.random();
+        // Method reference way
+        Supplier<Double> supplier1 = Math :: random;
 
     }
 
