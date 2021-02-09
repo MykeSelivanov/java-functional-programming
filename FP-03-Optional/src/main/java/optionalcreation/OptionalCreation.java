74ae6efdd48857a93a6df7683666faa59ea4f67d
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-08 23:52:10 -0600

    Optional

--- FP-03-Optional/src/main/java/optionalcreation/OptionalCreation.java
+++ FP-03-Optional/src/main/java/optionalcreation/OptionalCreation.java
@@ -5,6 +5,16 @@ import java.util.Optional;
 public class OptionalCreation {
     public static void main(String[] args) {
 
+        /**
+         * Optional is a separate object (new object will be created), that wraps the value you need
+         * It consumes 16 bytes
+         * Optionals are immutable - you can't change the value, once it's created
+         * Creating an optionals everywhere can cost performance
+         */
+
+        // 3 ways to create optional
+
+        // of method
         String str = "A String";
         Optional<String> optional = Optional.of(str);
 
