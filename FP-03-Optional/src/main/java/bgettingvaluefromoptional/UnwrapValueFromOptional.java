d54907062c7c41ad835580f9b435749554c70840
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-11 21:58:19 -0600

    Optional

--- FP-03-Optional/src/main/java/bgettingvaluefromoptional/UnwrapValueFromOptional.java
+++ FP-03-Optional/src/main/java/bgettingvaluefromoptional/UnwrapValueFromOptional.java
@@ -1,9 +1,16 @@
 package bgettingvaluefromoptional;
 
+import java.util.Optional;
+
 public class UnwrapValueFromOptional {
     public static void main(String[] args) {
 
+        Integer a = 10;
+        Optional<Integer> optional = Optional.of(a);
 
+        // get
+        Integer intValue = optional.get();
+        System.out.println(intValue);
 
     }
 
