c60309271d482aac96b4f212d07b30c398d2a4e5
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-11 22:01:27 -0600

    Optional

--- FP-03-Optional/src/main/java/bgettingvaluefromoptional/UnwrapValueFromOptional.java
+++ FP-03-Optional/src/main/java/bgettingvaluefromoptional/UnwrapValueFromOptional.java
@@ -8,9 +8,16 @@ public class UnwrapValueFromOptional {
         Integer a = 10;
         Optional<Integer> optional = Optional.of(a);
 
-        // get
+        // get - risky if you're not sure that optional has value
         Integer intValue = optional.get();
         System.out.println(intValue);
+
+        Optional<Integer> emptyOptional = Optional.empty();
+//        emptyOptional.get(); // throws NoSuchElement exception
+
+        // isPresent() - returns a boolean
+        Integer val = optional.isPresent() ? optional.get() : 0; // zero will become default value
+        System.out.println(val);
 
     }
 
