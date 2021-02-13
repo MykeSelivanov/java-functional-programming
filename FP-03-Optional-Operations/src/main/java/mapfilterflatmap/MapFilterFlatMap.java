872de058424cfd8270f8c79c01c0a62363e55b1c
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-12 20:40:10 -0600

    Optional Operations

--- FP-03-Optional-Operations/src/main/java/mapfilterflatmap/MapFilterFlatMap.java
+++ FP-03-Optional-Operations/src/main/java/mapfilterflatmap/MapFilterFlatMap.java
@@ -8,14 +8,25 @@ public class MapFilterFlatMap {
         Optional<String> optional = Optional.of("Value");
         Optional<String> emptyOptional = Optional.empty();
 
-        // map()
+        // map() - modifies the value of optional
         Optional<String> map = optional.map(val -> "Replaced");
         System.out.println(map.get());
 
         Optional<String> mapOnEmpty = emptyOptional.map(val -> "New value");
 //        System.out.println(mapOnEmpty.get()); // will throw no such element exception
-        String mapOnEmpty1 = mapOnEmpty.map(val -> "Proper way").orElse("Value From Map");
+        String mapOnEmpty1 = mapOnEmpty.map(val -> "Proper way").orElse("Optional was empty, here is Default value");
         System.out.println(mapOnEmpty1);
+
+        // filter() - checks the condition for optional with a predicate
+        // if condition is satisfied, the same optional again will be returned
+        Optional<String> filter = optional.filter(val -> val.equalsIgnoreCase("Value"));
+        System.out.println(filter.get());
+
+        // if condition is not satisfied, empty optional will be returned
+        Optional<String> filter1 = optional.filter(val -> val.equalsIgnoreCase("NOT VALUE"));
+        System.out.println(filter1.orElse("Default"));
+
+
 
 
     }
