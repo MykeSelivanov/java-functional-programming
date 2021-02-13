7b540e0da45ad2041d95ffce7308a04774582faa
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-12 20:34:24 -0600

    Optional Operations

--- FP-03-Optional-Operations/src/main/java/mapfilterflatmap/MapFilterFlatMap.java
+++ FP-03-Optional-Operations/src/main/java/mapfilterflatmap/MapFilterFlatMap.java
@@ -1,4 +1,22 @@
 package mapfilterflatmap;
 
+import java.util.Optional;
+
 public class MapFilterFlatMap {
+    public static void main(String[] args) {
+
+        Optional<String> optional = Optional.of("Value");
+        Optional<String> emptyOptional = Optional.empty();
+
+        // map()
+        Optional<String> map = optional.map(val -> "Replaced");
+        System.out.println(map.get());
+
+        Optional<String> mapOnEmpty = emptyOptional.map(val -> "New value");
+//        System.out.println(mapOnEmpty.get()); // will throw no such element exception
+        String mapOnEmpty1 = mapOnEmpty.map(val -> "Proper way").orElse("Value From Map");
+        System.out.println(mapOnEmpty1);
+
+
+    }
 }
