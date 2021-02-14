bb4ac2bf15c5803750856f3e622eff199da9623e
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-14 15:24:54 -0600

    Optional Operations

--- FP-03-Optional-Operations/src/main/java/ifpresentstreamorequalshashcode/IfPresentStreamOrEqualsHashcode.java
+++ FP-03-Optional-Operations/src/main/java/ifpresentstreamorequalshashcode/IfPresentStreamOrEqualsHashcode.java
@@ -10,6 +10,15 @@ public class IfPresentStreamOrEqualsHashcode {
         optional.ifPresent(System.out :: println);
         optional.ifPresent(val -> System.out.println("This is a " + val));
 
+        // ifPresentOrElse(Consumer<? super T> action, Runnable emptyAction)
+        // If a value is present, performs the given action with the value, otherwise performs the given empty-based action.
+        Optional.empty().ifPresentOrElse(System.out :: println, () -> System.out.println("Value is absent"));
+
+        // stream()
+        // If a value is present, returns a sequential Stream containing only that value,
+        // otherwise returns an empty Stream
+        optional.stream()
+
     }
 
 }
