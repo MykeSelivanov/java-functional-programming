6b6060e0475dd72337111948415335fa87e49c4b
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-15 23:11:21 -0600

    Pure Functions

--- FP-04-Functional-Concepts-Pure-Functions-No-Side-Effects/src/main/java/purefunctions/ImpureFunction.java
+++ FP-04-Functional-Concepts-Pure-Functions-No-Side-Effects/src/main/java/purefunctions/ImpureFunction.java
@@ -4,6 +4,8 @@ public class ImpureFunction {
 
     private int value = 0;
 
+    // Method add uses a member variable to calculate it's return value
+    // it also modifies the state of value member variable - it has a side effect
     public int add(int nextValue){
         this.value += nextValue;
         return this.value;
