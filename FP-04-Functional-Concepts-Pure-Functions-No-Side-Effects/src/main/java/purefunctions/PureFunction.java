a3af0fd41264e8e617cd9daafa56248d3918be92
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-18 23:40:27 -0600

    Pure functions

--- FP-04-Functional-Concepts-Pure-Functions-No-Side-Effects/src/main/java/purefunctions/PureFunction.java
+++ FP-04-Functional-Concepts-Pure-Functions-No-Side-Effects/src/main/java/purefunctions/PureFunction.java
@@ -7,8 +7,17 @@ public class PureFunction {
 
     // No side effects
     // function does not modify any state outside the function anywhere
-    public int sum(int a, int b){
+    public static int sum(int a, int b){
         return a + b;
+    }
+
+    public static int multiply(int a, int b){
+        log(String.format("Returning %s as the result of %s * %s", a * b, a, b));
+        return a * b;
+    }
+
+    public static void log(String message){
+        System.out.println(message);
     }
 
 }
