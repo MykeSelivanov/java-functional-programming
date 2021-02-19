2b6cf32106b07c1be143bae59322798d57c56751
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-18 23:53:41 -0600

    Pure functions

---
+++ FP-04-Functional-Concepts-Higher-Order-Functions/src/main/java/higherorderfunctions/HigherOrderFunctions.java
@@ -0,0 +1,17 @@
+package higherorderfunctions;
+
+public class HigherOrderFunctions {
+    public static void main(String[] args) {
+
+        /**
+         * Higher order functions are functions which either:
+         * take functions as arguments
+         * or return functions as output
+         * or does both
+         */
+
+
+
+    }
+
+}
