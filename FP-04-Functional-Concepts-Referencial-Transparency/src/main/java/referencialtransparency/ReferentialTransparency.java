eb988e6639bc1610dd35ca4b9c4e1cea9e069089
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-26 22:08:50 -0600

    Referential Transparency

---
+++ FP-04-Functional-Concepts-Referencial-Transparency/src/main/java/referencialtransparency/ReferentialTransparency.java
@@ -0,0 +1,14 @@
+package referencialtransparency;
+
+public class ReferentialTransparency {
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+
+
+}
