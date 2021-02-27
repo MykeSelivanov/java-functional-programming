85c566d86dc904c24dfd292bf7834c6bc80c113d
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-26 22:16:02 -0600

    Referential Transparency

--- FP-04-Functional-Concepts-Referencial-Transparency/src/main/java/referencialtransparency/ReferentialTransparency.java
+++ FP-04-Functional-Concepts-Referencial-Transparency/src/main/java/referencialtransparency/ReferentialTransparency.java
@@ -3,11 +3,14 @@ package referencialtransparency;
 public class ReferentialTransparency {
     public static void main(String[] args) {
 
-        /** Referential transparency means that a function call can be replaced bu it's value
+        /** Referential transparency means that a function call can be replaced b it's value
          * or another referentially transparent call with the same result
          */
 
         int result = add(2, multiply(2,4));
+        // multiply(2,4) is referentially transparent method
+        // you can substitute multiply(2,4) with 8 or
+        // you can substitute multiply(2,4) with multiply(2, multiply(2,2))
         System.out.println(result);
 
 
