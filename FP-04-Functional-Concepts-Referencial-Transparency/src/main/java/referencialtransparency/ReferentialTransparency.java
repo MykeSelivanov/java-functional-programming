8fb845695f53968cccd0431225d639a441821b71
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-26 22:23:19 -0600

    Referential Transparency

--- FP-04-Functional-Concepts-Referencial-Transparency/src/main/java/referencialtransparency/ReferentialTransparency.java
+++ FP-04-Functional-Concepts-Referencial-Transparency/src/main/java/referencialtransparency/ReferentialTransparency.java
@@ -5,6 +5,10 @@ public class ReferentialTransparency {
 
         /** Referential transparency means that a function call can be replaced b it's value
          * or another referentially transparent call with the same result
+         * Functional style avoids such methods that are not referentially transparent
+         * And the reason functional programming use referential transparency is that
+         * it makes reasoning about programs easier, it also makes each subprogram independent
+         * which greatly simplifies unit testing and refactoring
          */
 
         int result = add(2, multiply(2,4));
@@ -19,6 +23,9 @@ public class ReferentialTransparency {
     public static int add(int a, int b){
         return a + b;
     }
+    // add(a,b) is also referentially transparent
+    // it is a pure function and all pure functions are referentially transparent
+    // however not all referentially transparent functions are pure functions
 
     public static int multiply(int a, int b){
         return a * b;
