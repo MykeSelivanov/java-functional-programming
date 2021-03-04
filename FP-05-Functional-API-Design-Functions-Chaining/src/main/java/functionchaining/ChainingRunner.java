ea51ba5318992d69bfebbdc9a9e79de3c06055ce
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-03 23:43:24 -0600

    Functions Chaining

--- FP-05-Functional-API-Design-Functions-Chaining/src/main/java/functionchaining/ChainingRunner.java
+++ FP-05-Functional-API-Design-Functions-Chaining/src/main/java/functionchaining/ChainingRunner.java
@@ -1,15 +1,30 @@
 package functionchaining;
 
 public class ChainingRunner {
+    public static void main(String[] args) {
 
-    /**
-     * Function chaining is a technic to chain functions
-     * Each function returns an object, allowing the calls to be chained together in a single statement
-     * without requiring variables to store the intermediate results
-     * Basically chaining is a technique used to simplify code, when multiple functions are applied in a row
-     */
 
-    Consumer<String> consumer = s -> System.out.println(s);
-    Consumer<String> consumer2 = s -> System.out.println(s);
+        /**
+         * Function chaining is a technic to chain functions
+         * Each function returns an object, allowing the calls to be chained together in a single statement
+         * without requiring variables to store the intermediate results
+         * Basically chaining is a technique used to simplify code, when multiple functions are applied in a row
+         */
 
+        Consumer<String> consumer1 = s -> System.out.println(s);
+        Consumer<String> consumer2 = s -> System.out.println(s);
+
+//        consumer1.accept("Hello");
+//        consumer2.accept("Hello");
+
+        Consumer<String> consumer3 = s -> {
+            consumer1.accept(s);
+            consumer2.accept(s);
+        };
+        consumer3.accept("Hello");
+
+        Consumer<String>
+
+
+    }
 }
