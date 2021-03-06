81d2ec727e7e9b4ca9994e26ea010753725dd5ee
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-05 22:07:41 -0600

    Functional Composition

--- FP-05-Functional-API-Design-Functional-Composition/src/main/java/Function.java
+++ FP-05-Functional-API-Design-Functional-Composition/src/main/java/Function.java
@@ -4,7 +4,7 @@ public interface Function<T, R> {
 
     // this method returns a function that can compose 2 functions
     default <V> Function<V,R> compose(Function<V,T> before) {
-        return (V v) -> ;
+        return (V v) -> apply(before.apply(v));
     }
 
 }
