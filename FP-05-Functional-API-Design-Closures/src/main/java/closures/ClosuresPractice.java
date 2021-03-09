cca50cc6ea068a255350ee9af4ea751d077a9854
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-08 23:07:24 -0600

    Closures

---
+++ FP-05-Functional-API-Design-Closures/src/main/java/closures/ClosuresPractice.java
@@ -0,0 +1,12 @@
+package closures;
+
+public class ClosuresPractice {
+    public static void main(String[] args) {
+
+        /**
+         * A closure is a function that refers to free variables in its lexical context
+         */
+
+    }
+
+}
