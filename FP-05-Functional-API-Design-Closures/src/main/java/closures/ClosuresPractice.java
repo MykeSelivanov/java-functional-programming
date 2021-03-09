94788829f1a042d7cef4e791dd396b3286be5909
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-08 23:10:48 -0600

    Closures

--- FP-05-Functional-API-Design-Closures/src/main/java/closures/ClosuresPractice.java
+++ FP-05-Functional-API-Design-Closures/src/main/java/closures/ClosuresPractice.java
@@ -5,6 +5,11 @@ public class ClosuresPractice {
 
         /**
          * A closure is a function that refers to free variables in its lexical context
+         * function - block of code, it may produce a result
+         * free variable - is an identifier used, but not defined by the closure
+         * lexical context - convention that sets the scope of a variable so that it may only be called within the block
+         * of code in which it is defined
+         *                                                            Neil Gafter (pioneered introducing closures in java)
          */
 
     }
