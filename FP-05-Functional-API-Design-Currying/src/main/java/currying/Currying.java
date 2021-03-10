45dc5c2e2df1ef394ada2dfc6ba02edde3dca0ef
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-09 21:57:54 -0600

    Currying

--- FP-05-Functional-API-Design-Currying/src/main/java/currying/Currying.java
+++ FP-05-Functional-API-Design-Currying/src/main/java/currying/Currying.java
@@ -13,12 +13,17 @@ public class Currying {
          * func1(par1) -> func2(par2) -> par1 + par2 -> result
          */
 
+        // takes integer and returns function
         Function <Integer, Function<Integer,Integer>> func1 = val -> val2 -> val + val2;
 
+        // long syntax
         Function<Integer, Integer> func2 = func1.apply(1);
-
         Integer sum = func2.apply(2);
         System.out.println(sum);
+
+        // short syntax
+        Integer sum2 = func1.apply(3).apply(4);
+        System.out.println(sum2);
 
     }
 
