866f4de4223340af4f1bfca4ccce3a604941a352
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-09 21:45:25 -0600

    Closures

--- FP-05-Functional-API-Design-Closures/src/main/java/closures/JavaClosure.java
+++ FP-05-Functional-API-Design-Closures/src/main/java/closures/JavaClosure.java
@@ -11,19 +11,25 @@ public class JavaClosure {
          * of code in which it is defined
          *                                                            Neil Gafter (pioneered introducing closures in java)
          */
+        // Closures are important because they control what is and what is not in scope of particular function
+        // along with which variables are shared between sibling functions in the same containing scope
 
         int value = 123;
 
+        // this lambda is a closure that uses a free variable present in it's lexical scope - value
         Task lambda = () -> {
+            // JRE is keeping track of value here
             System.out.println(value);
             System.out.println("Task Completed");
         };
 
+        // when we pass lambda here, the variable value is also passed
         printValue(lambda);
 
     }
 
     private static void printValue(Task lambda){
+        // where we calling lambda, the value variable is not present in this scope
         lambda.doTask();
     }
 
