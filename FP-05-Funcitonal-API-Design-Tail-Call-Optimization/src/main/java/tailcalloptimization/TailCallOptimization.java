9818e7526021e5c6bb680b4b20319941e5218346
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-10 23:39:45 -0600

    Tail Call Optimization

--- FP-05-Funcitonal-API-Design-Tail-Call-Optimization/src/main/java/tailcalloptimization/TailCallOptimization.java
+++ FP-05-Funcitonal-API-Design-Tail-Call-Optimization/src/main/java/tailcalloptimization/TailCallOptimization.java
@@ -5,13 +5,22 @@ public class TailCallOptimization {
 
         /**
          * Tail Call Optimization or Tail Call Recursion
+         * In case of regular recursion it keeps holding onto the stack because there are further operations
          */
 
     }
 
+    // regular recursion
     public static long recursiveFactorial(int n){
         if (n <= 1) return 1;
         return n * recursiveFactorial(n - 1);
+    }
+
+    // Tail recursion
+    // n - is the number, whose factorial we need to calculate and a - is accumulator to accumulate the calculation product
+    public static long tailReFact(int n, int a){
+        if (n <= 1) return a;
+        return tailReFact(n-1, n * a);
     }
 
 }
