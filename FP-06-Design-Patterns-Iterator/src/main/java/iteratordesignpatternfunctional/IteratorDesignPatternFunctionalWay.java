1afd75c293d8945ec1d331ce6d7a161e4003393b
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-12 21:41:19 -0600

    Iterator Functional way

---
+++ FP-06-Design-Patterns-Iterator/src/main/java/iteratordesignpatternfunctional/IteratorDesignPatternFunctionalWay.java
@@ -0,0 +1,18 @@
+package iteratordesignpatternfunctional;
+
+public class IteratorDesignPatternFunctionalWay {
+    public static void main(String[] args) {
+
+        /**
+         * Iterator pattern is about accessing elements without exposing internal structure
+         *
+         * The iterator design pattern enables to get a way to access the elements of a collection object in sequential
+         * manner without any need to know its internal representation
+         * Examples:
+         * java.util.Iterator
+         * java.util.Enumeration
+         */
+
+    }
+
+}
