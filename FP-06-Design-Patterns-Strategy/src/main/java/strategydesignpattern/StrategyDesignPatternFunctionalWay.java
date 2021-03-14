e11a62f54fb80e3a205e53d3cd83785f94b498be
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-14 12:11:15 -0500

    Strategy Functional

---
+++ FP-06-Design-Patterns-Strategy/src/main/java/strategydesignpattern/StrategyDesignPatternFunctionalWay.java
@@ -0,0 +1,12 @@
+package strategydesignpattern;
+
+public class StrategyDesignPatternFunctionalWay {
+    public static void main(String[] args) {
+        /**
+         * Strategy design pattern is used when we have multiple solutions or algorithms to solve a specific task
+         * and client decides the specific implementation which is to be used in runtime
+         */
+
+
+    }
+}
