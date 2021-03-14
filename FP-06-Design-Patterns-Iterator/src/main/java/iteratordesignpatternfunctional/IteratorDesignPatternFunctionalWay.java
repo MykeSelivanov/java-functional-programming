87044962192627a79eed67dbbe6096cbee18552c
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-13 19:01:44 -0600

    Iterator Functional way

--- FP-06-Design-Patterns-Iterator/src/main/java/iteratordesignpatternfunctional/IteratorDesignPatternFunctionalWay.java
+++ FP-06-Design-Patterns-Iterator/src/main/java/iteratordesignpatternfunctional/IteratorDesignPatternFunctionalWay.java
@@ -14,6 +14,13 @@ public class IteratorDesignPatternFunctionalWay {
          *
          */
 
+        //starting from java8 there is a functional forEach method, that iterates over collections
+
+        MyArrayListSimple myArrayListSimple = new MyArrayListSimple(new Object[] {1,2,3,4,5});
+
+        // functional implementation of iterator with our custom forEach method
+        // will print all the object elements of list
+        myArrayListSimple.forEach(System.out::println);
 
 
     }
