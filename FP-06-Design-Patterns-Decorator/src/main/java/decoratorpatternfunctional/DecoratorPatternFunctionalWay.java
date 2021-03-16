08c832e9a653435ccc6e610c5be48f06299169aa
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-15 20:15:12 -0500

    Decorator Functional way

---
+++ FP-06-Design-Patterns-Decorator/src/main/java/decoratorpatternfunctional/DecoratorPatternFunctionalWay.java
@@ -0,0 +1,18 @@
+package decoratorpatternfunctional;
+
+public class DecoratorPatternFunctionalWay {
+    public static void main(String[] args) {
+
+        /**
+         * The Decorator design pattern is used to modify functionality of object at runtime without affecting other
+         * instances of the same class
+         *
+         * Basically this pattern provides functionality wrappers and we keep on wrapping the object into these wrappers
+         * one after another according to the additional decorations or the additional functionalities
+         * Also each new functionality demands new class for itself
+         */
+
+
+    }
+
+}
