878d2495b68dda9dc57bcf96aa64e120a07950a3
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-15 22:18:40 -0500

    Fluent interface

---
+++ FP-06-Design-Patterns-Fluent-Interfaces/src/main/java/fluentinterfaces/FluentShopping.java
@@ -0,0 +1,12 @@
+package fluentinterfaces;
+
+public class FluentShopping {
+    public static void main(String[] args) {
+
+        /**
+         * A fluent interface provides an easy-readable, flowing interface, that often mimics a domain specific
+         * language. Using this pattern results in code that can be read nearly as human language.
+         */
+
+    }
+}
