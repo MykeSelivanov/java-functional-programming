bb3f1b439e63684026fcdb3eb95e0bb047b18721
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-17 22:41:03 -0500

    Factory Functional way

--- FP-06-Design-Patterns-Factory/src/main/java/factoryfunctionalway/FactoryDesignPatternFunctionalWay.java
+++ FP-06-Design-Patterns-Factory/src/main/java/factoryfunctionalway/FactoryDesignPatternFunctionalWay.java
@@ -2,7 +2,21 @@ package factoryfunctionalway;
 
 public class FactoryDesignPatternFunctionalWay {
     public static void main(String[] args) {
-
+        /**
+         * Factory is a Creational pattern
+         * Factory pattern is about creating object without exposing instantiation logic, so client knows nothing
+         * about it, even the type of the object is not known
+         *
+         * Ex:
+         * Calendar cal = Calendar.getInstance(); -> returns Calendar instance
+         * System.out.println(cal); -> java.util.GregorianCalendar - GregorianCalendar is the actual concrete implementation
+         * that is being called underneath this factory instance
+         *
+         * The factory design pattern is a way to instantiating a class inside a designated method that we call a
+         * factory method.
+         * Factory is an object that is able to create other objects
+         *
+         */
 
 
     }
