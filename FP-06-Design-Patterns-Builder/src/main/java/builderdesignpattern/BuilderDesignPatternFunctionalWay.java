d9ba762bbe7a53b088e387ea9dc000cdd6cc636a
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-19 22:18:07 -0500

    Builder Functional way

--- FP-06-Design-Patterns-Builder/src/main/java/builderdesignpattern/BuilderDesignPatternFunctionalWay.java
+++ FP-06-Design-Patterns-Builder/src/main/java/builderdesignpattern/BuilderDesignPatternFunctionalWay.java
@@ -3,6 +3,10 @@ package builderdesignpattern;
 public class BuilderDesignPatternFunctionalWay {
     public static void main(String[] args) {
 
+        /**
+         * Builder Design pattern is all about providing flexibility in Object Creation. The intent of the builder
+         * design pattern is to isolate the construction of a complex object from its representation.
+         */
 
 
     }
