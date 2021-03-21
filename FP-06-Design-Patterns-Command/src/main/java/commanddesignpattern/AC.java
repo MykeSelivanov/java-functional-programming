5ab4c2072e0d9b89b877fcea3779d673f1a138b5
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-21 12:00:29 -0500

    Command Functional way

---
+++ FP-06-Design-Patterns-Command/src/main/java/commanddesignpattern/AC.java
@@ -0,0 +1,21 @@
+package commanddesignpattern;
+
+public class AC {
+
+    public void turnOn(){
+        System.out.println("Turning on AC");
+    }
+
+    public void turnOff(){
+        System.out.println("Turning off AC");
+    }
+
+    public void incTemp(){
+        System.out.println("Increasing temperature");
+    }
+
+    public void decTemp(){
+        System.out.println("Decreasing temperature");
+    }
+
+}
