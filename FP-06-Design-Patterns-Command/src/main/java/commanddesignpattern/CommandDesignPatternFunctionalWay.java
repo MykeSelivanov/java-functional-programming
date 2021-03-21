a48300d790820b64e910649510ab2ee052af3e20
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-20 20:30:59 -0500

    Command Functional way

---
+++ FP-06-Design-Patterns-Command/src/main/java/commanddesignpattern/CommandDesignPatternFunctionalWay.java
@@ -0,0 +1,20 @@
+package commanddesignpattern;
+
+public class CommandDesignPatternFunctionalWay {
+    public static void main(String[] args) {
+
+        /**
+         * Command is a behavioral design pattern, which is about encapsulating a request as an object
+         * In this we write code that sequences and executes methods based on runtime decisions (commands)
+         *
+         * For ex. all the implementations of Runnable interface (java.land.Runnable), like creating thread by
+         * implementing runnable so each implementation that we give to runnable is a different command on which
+         * the methods of runnable gets executed
+         *
+         * Command pattern is a way to encapsulate a request as an object, parametrize clients with different requests and
+         * perform corresponding operations
+         */
+
+    }
+
+}
