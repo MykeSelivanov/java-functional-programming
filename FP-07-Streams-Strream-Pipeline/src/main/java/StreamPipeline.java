dc82a69ff7bf6bc38d44be33674e4e69aefb3b7e
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-22 22:10:57 -0500

    Stream Pipeline

--- FP-07-Streams-Strream-Pipeline/src/main/java/StreamPipeline.java
+++ FP-07-Streams-Strream-Pipeline/src/main/java/StreamPipeline.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StreamPipeline {
+    public static void main(String[] args) {
+
+        List<Book> books = new ArrayList<>();
+
+        List<Book> collect = books.stream()
+                .filter(book -> book.getGenre().equalsIgnoreCase("Horror"))
+                .filter(book -> book.getRating() > 3)
+                .collect(Collectors.toList());
+
+        // 1.
+        Stream<Book> stream = books.stream();
+
+        // 2.
+        Stream<Book> horrorBooks = stream.filter(book -> book.getGenre().equalsIgnoreCase("Horror"));
+
+        // 3.
+        Stream<Book> popularHorrorBooks = horrorBooks.filter(book -> book.getRating() > 3);
+
+        // 4.
+        List<Book> collect1 = popularHorrorBooks.collect(Collectors.toList());
+
+
+    }
 }
