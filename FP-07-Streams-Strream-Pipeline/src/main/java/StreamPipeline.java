2e3e97a0dd327eba781498c4d184df8ceba7f80d
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-22 22:15:24 -0500

    Stream Pipeline

--- FP-07-Streams-Strream-Pipeline/src/main/java/StreamPipeline.java
+++ FP-07-Streams-Strream-Pipeline/src/main/java/StreamPipeline.java
@@ -6,8 +6,17 @@ import java.util.stream.Stream;
 public class StreamPipeline {
     public static void main(String[] args) {
 
+        /**
+         * Stream pipeline consists of a:
+         * 1. Source - collection, an array, a generator function or an i/o channel
+         * 2. Zero or more Intermediate Operations - operations that are applied on a stream and they return another stream
+         * 3. Terminal operation - produce a result or side effect from the stream
+         * (side effect is anything other than returning a result, for eg print statement)
+         */
+
         List<Book> books = new ArrayList<>();
 
+        // Stream pipeline
         List<Book> collect = books.stream()
                 .filter(book -> book.getGenre().equalsIgnoreCase("Horror"))
                 .filter(book -> book.getRating() > 3)
