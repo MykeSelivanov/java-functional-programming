9f59e0c10eda02f3aee901e292ed4ab18c867296
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-24 22:42:13 -0500

    Map

--- FP-07-Streams-03-Map/src/main/java/mapoperation/StreamsMapOperation.java
+++ FP-07-Streams-03-Map/src/main/java/mapoperation/StreamsMapOperation.java
@@ -1,8 +1,18 @@
 package mapoperation;
 
+import java.util.stream.Stream;
+
 public class StreamsMapOperation {
     public static void main(String[] args) {
 
+        /**
+         * Stream map(Function mapper)
+         * Map operation takes a mapper function, which is basically a function with abstract method 'R apply(T t);'
+         *
+         * Map function takes each stream element and map it to some other value returned by the mapper function
+         */
+
+        Stream.of(1,2,3,4,5,6,7,8,9,10);
 
 
     }
