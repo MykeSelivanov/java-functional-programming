78b8025e13dca790b261bc616ab2d192dd07d716
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-25 23:13:30 -0500

    Reduce

---
+++ FP-07-Streams-03-Reduce/src/main/java/reduceoperation/StreamsReduceOperation.java
@@ -0,0 +1,13 @@
+package reduceoperation;
+
+public class StreamsReduceOperation {
+    public static void main(String[] args) {
+
+        /**
+         * Reducing is repeated process of combining all the elements
+         */
+
+
+    }
+
+}
