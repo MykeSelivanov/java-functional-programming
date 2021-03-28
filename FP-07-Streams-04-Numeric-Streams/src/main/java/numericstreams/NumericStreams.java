b8735e30fea3ae03b3d8e53b1a784c8e3db8a4da
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-28 17:30:26 -0500

    Numeric Streams

--- FP-07-Streams-04-Numeric-Streams/src/main/java/numericstreams/NumericStreams.java
+++ FP-07-Streams-04-Numeric-Streams/src/main/java/numericstreams/NumericStreams.java
@@ -3,6 +3,10 @@ package numericstreams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class NumericStreams {
     public static void main(String[] args) {
@@ -10,6 +14,16 @@ public class NumericStreams {
         /**
          * IntStream, LongStream, DoubleStream - primitive streams in java 8
          *
+         * map() vs mapToDouble()
+         * map : stream of Objects, if the result is primitive it will box it to Object (for eg Integer, Double, Long)
+         * mapToDouble : unbox the Object to primitive
+         *
+         * IntStream marToInt(toIntFunction mapper) : Integer -> int
+         * LongStream marToInt(toLongFunction mapper) : Long -> long
+         * DoubleStream marToInt(toDoubleFunction mapper) : Double -> double
+         *
+         * One of the biggest benefits of using primitive streams is that they allow to avoid the cost of boxing and unboxing
+         * which means better performance
          *
          */
 
@@ -26,6 +40,18 @@ public class NumericStreams {
                 .average(); // returns an optional
 
         System.out.println(averageRating.getAsDouble());
+
+        // of() method for stream creation in primitive streams
+        IntStream intS = IntStream.of(1,2,3,4,5);
+        DoubleStream doubleS = DoubleStream.of(1.501, 2.367, 3.475, 4.599);
+        LongStream longS = LongStream.of(1L ,2L, 3L, 4L, 5L);
+
+        // boxed method is boxing stream primitives to objects
+        Stream<Integer> boxedInts = intS.boxed(); // returns the object version of stream with Integers
+
+        // matToObj() also maps the primitives in streams to objects, you need to provide a mapper function
+        Stream<Double> boxedDoubles = doubleS.mapToObj(value -> value); // first value is primitive, second one is mapped to an object (Double in this case)
+
 
     }
 
