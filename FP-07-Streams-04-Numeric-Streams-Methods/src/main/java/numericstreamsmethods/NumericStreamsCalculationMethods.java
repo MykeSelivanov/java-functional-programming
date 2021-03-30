40c605a5298fc0538d56a6880a4024e5893f4ae5
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-29 23:16:43 -0500

    Numeric Streams - Methods

--- FP-07-Streams-04-Numeric-Streams-Methods/src/main/java/numericstreamsmethods/NumericStreamsCalculationMethods.java
+++ FP-07-Streams-04-Numeric-Streams-Methods/src/main/java/numericstreamsmethods/NumericStreamsCalculationMethods.java
@@ -1,5 +1,6 @@
 package numericstreamsmethods;
 
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 public class NumericStreamsCalculationMethods {
@@ -13,6 +14,18 @@ public class NumericStreamsCalculationMethods {
         int sum = IntStream.of(1,2,3,4,5)
                 .sum();
         System.out.println(sum);
+
+        // max() - returns an optional, but primitive version of optional
+        // in case of empty stream, empty optional will be returned
+        OptionalInt maxOptional = IntStream.of(5,6,7,8)
+                .max();
+        System.out.println(maxOptional.getAsInt());
+
+        // min() - also returns a primitive optional
+        OptionalInt minOptional = IntStream.of(3,4,-5)
+                .min();
+        System.out.println(minOptional.getAsInt());
+
 
     }
 
