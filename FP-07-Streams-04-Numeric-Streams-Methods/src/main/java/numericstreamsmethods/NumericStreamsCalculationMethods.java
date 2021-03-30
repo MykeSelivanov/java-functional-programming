faa15814aa89688635a618506985a880f968eb48
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-29 23:12:24 -0500

    Numeric Streams - Methods

--- FP-07-Streams-04-Numeric-Streams-Methods/src/main/java/numericstreamsmethods/NumericStreamsCalculationMethods.java
+++ FP-07-Streams-04-Numeric-Streams-Methods/src/main/java/numericstreamsmethods/NumericStreamsCalculationMethods.java
@@ -3,6 +3,9 @@ package numericstreamsmethods;
 public class NumericStreamsCalculationMethods {
     public static void main(String[] args) {
 
+        // IntStream
+        // LongStream
+        // DoubleStream
 
 
     }
