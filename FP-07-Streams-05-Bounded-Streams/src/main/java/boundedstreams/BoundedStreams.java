5e6a549e83db7b671b371386b331da74491fba67
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-31 19:00:33 -0500

    Bounded Streams

--- FP-07-Streams-05-Bounded-Streams/src/main/java/boundedstreams/BoundedStreams.java
+++ FP-07-Streams-05-Bounded-Streams/src/main/java/boundedstreams/BoundedStreams.java
@@ -13,26 +13,28 @@ public class BoundedStreams {
          * Some of the approaches to build finite or bounded streams
          */
 
+        // 1. stream() on a Collection
         List<Integer> list = List.of(1,4,7,9,4);
         Stream<Integer> streamIntegers = list.stream();
 
+        // how you can stream a Map
         Map<Integer,String> map = Map.of(1, "one", 2, "two", 3, "three", 4, "four");
         Stream<Map.Entry<Integer,String>> entriesStream = map.entrySet().stream();
 
         Stream<String> valuesStream = map.values().stream();
         Stream<Integer> keysStream = map.keySet().stream();
 
-        // of() method
+        // 2. static of() method of Stream class
         Stream<String> streamOfStrings = Stream.of("Hi", "Hey", "Hello");
 
-        // stream method on arrays class
+        // 3. stream() method of Arrays utility class
         Integer [] integerArray = {1,2,3,4,5,6};
         Stream<Integer> streamOfIntegersFromArray = Arrays.stream(integerArray);
 
         int [] ints = {7,8,9,10};
         IntStream streamOfPrimitiveInts = Arrays.stream(ints);
 
-        // using builder pattern - build Stream
+        // 4. using builder pattern - build Stream
         Stream.Builder<Integer> builder = Stream.builder();
         builder.add(1);
 
