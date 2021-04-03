210ed260cffa8fa00fd763bb68b2268a18c02831
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-02 23:14:09 -0500

    Parallel Streams

---
+++ FP-07-Streams-07-Parallel-Streams/src/main/java/parallelstreams/ParallelStreams.java
@@ -0,0 +1,18 @@
+package parallelstreams;
+
+public class ParallelStreams {
+    public static void main(String[] args) {
+        /**
+         *
+         *
+         * There are 2 ways of processing with streams:
+         * Sequential - <collection>.stream() - streams are always sequential by default
+         * Parallel - to create a parallel Stream:
+         *                                          - <collection>.parallelStream() method or
+         *                                          - <stream>.parallel() - invoke a parallel() method on a regular sequential stream
+         */
+
+
+    }
+
+}
