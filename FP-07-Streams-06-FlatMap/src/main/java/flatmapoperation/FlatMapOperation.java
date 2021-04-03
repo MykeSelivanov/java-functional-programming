5baa68e23f2484f6cc98a7fc3c633c85fe8d22aa
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-02 19:10:12 -0500

    FlatMap Operation

--- FP-07-Streams-06-FlatMap/src/main/java/flatmapoperation/FlatMapOperation.java
+++ FP-07-Streams-06-FlatMap/src/main/java/flatmapoperation/FlatMapOperation.java
@@ -1,5 +1,12 @@
 package flatmapoperation;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FlatMapOperation {
@@ -17,6 +24,22 @@ public class FlatMapOperation {
         // identity function is a function that takes an element, and returns the same element
         Stream<String> flatMap = Stream.of(a, b)
                 .flatMap(stringStream -> stringStream);
+
+        // store the "text.txt" file path in Path variable from the java.nio.file library
+        Path p = Paths.get("/Users/mykhailoselivanov/Documents/java-projects/java-functional-programming/FP-07-Streams-06-FlatMap/src/main/java/flatmapoperation/text.txt");
+
+        // now we have lines as a Stream elements
+        try (Stream<String> textLines = Files.lines(p);) {
+            // now we will collect all the words from each line, splitting them on a "/s"
+            List<String> textWords = textLines
+                    .flatMap(line -> Arrays.stream(line.split(" ")))
+                    .collect(Collectors.toList());
+
+            textWords.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
