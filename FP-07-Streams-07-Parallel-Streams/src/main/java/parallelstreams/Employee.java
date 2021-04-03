ad364fe6eb19303dcc0d8b79291b0e206047a4df
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-03 16:20:04 -0500

    Parallel Streams

---
+++ FP-07-Streams-07-Parallel-Streams/src/main/java/parallelstreams/Employee.java
@@ -0,0 +1,22 @@
+package parallelstreams;
+
+public class Employee {
+    private String name;
+    private int salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
