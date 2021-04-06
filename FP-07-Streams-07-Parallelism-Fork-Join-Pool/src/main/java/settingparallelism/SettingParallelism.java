93513375cd28368b892efdc13d3f390efa75039f
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-06 00:05:21 -0500

    Parallel Streams - Fork Join Pool

--- FP-07-Streams-07-Parallelism-Fork-Join-Pool/src/main/java/settingparallelism/SettingParallelism.java
+++ FP-07-Streams-07-Parallelism-Fork-Join-Pool/src/main/java/settingparallelism/SettingParallelism.java
@@ -15,8 +15,16 @@ public class SettingParallelism {
 
         // by setting this property you can control the number of cores that processor will use for ForkJoinPool
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism","2");
-        // will print the number of cores, that will be used by ForkJoinPool
+
+        // will print the number of cores, that will be used by ForkJoinPool, this means there will be 2 threads in this ForkJoinPool
+        // By default ForkJoinPool is using (n - 1) threads available in the system, because one thread is always required
+        // for the main method, therefore, if your system has 16 cores, ForkJoinPool will use 15
         System.out.println(ForkJoinPool.getCommonPoolParallelism());
+
+        // If you don't want to use common ForkJoinPool, you can create your own
+        ForkJoinPool pool = new ForkJoinPool(2); // parameter limits the number of threads used by this instance of ForkJoinPool
+
+
 
     }
 }
