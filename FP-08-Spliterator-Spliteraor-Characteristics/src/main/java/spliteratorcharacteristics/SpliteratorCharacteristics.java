bf482234773d632f20c44225e1338ff94ed42963
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-07 22:48:42 -0500

    Parallel Streams - Fork Join Pool

---
+++ FP-08-Spliterator-Spliteraor-Characteristics/src/main/java/spliteratorcharacteristics/SpliteratorCharacteristics.java
@@ -0,0 +1,15 @@
+package spliteratorcharacteristics;
+
+public class SpliteratorCharacteristics {
+    public static void main(String[] args) {
+
+        /**
+         * Spliterator (interface) is a special object, on which a stream is built, used to connect a stream on custom source
+         * Spliterator is also used for other sources, like collections
+         * Behind the scenes collections already have their own implementations of Spliterator, which used, when stream is created
+         * But custom source requires custom implementation
+         */
+
+    }
+
+}
