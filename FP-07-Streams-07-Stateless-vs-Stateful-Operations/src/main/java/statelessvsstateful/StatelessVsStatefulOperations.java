355e6267bdf537972d672e57a174f4cfd93962b3
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-08 23:51:56 -0500

    Stateless vs Stateful

--- FP-07-Streams-07-Stateless-vs-Stateful-Operations/src/main/java/statelessvsstateful/StatelessVsStatefulOperations.java
+++ FP-07-Streams-07-Stateless-vs-Stateful-Operations/src/main/java/statelessvsstateful/StatelessVsStatefulOperations.java
@@ -12,7 +12,6 @@ public class StatelessVsStatefulOperations {
          *
          * Stateful operation -
          *                      is one that uses outside info
-         *
          */
 
         List<Integer> nums = List.of(1,2,3,4,5,6,7,8,9);
