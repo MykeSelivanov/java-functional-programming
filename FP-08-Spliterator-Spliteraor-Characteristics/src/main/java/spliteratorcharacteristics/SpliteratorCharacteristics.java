6c587106b2029e2fa65bd46f069b4826ce7c2615
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-09 23:54:27 -0500

    Spliterator Characteristics

--- FP-08-Spliterator-Spliteraor-Characteristics/src/main/java/spliteratorcharacteristics/SpliteratorCharacteristics.java
+++ FP-08-Spliterator-Spliteraor-Characteristics/src/main/java/spliteratorcharacteristics/SpliteratorCharacteristics.java
@@ -1,5 +1,8 @@
 package spliteratorcharacteristics;
 
+import java.util.Spliterator;
+import java.util.Spliterators;
+
 public class SpliteratorCharacteristics {
     public static void main(String[] args) {
 
@@ -11,9 +14,18 @@ public class SpliteratorCharacteristics {
          *
          * Spliterator characteristics are used to define the state of the stream
          * int characteristics() method returns the set of characteristics of the spliterator and its elements
+         *
+         *  public static final int ORDERED    = 0x00000010;
+         *  public static final int DISTINCT   = 0x00000001;
+         *  public static final int SORTED     = 0x00000004;
+         *  public static final int SIZED      = 0x00000040;
+         *  public static final int NONNULL    = 0x00000100;
+         *  public static final int IMMUTABLE  = 0x00000400;
+         *  public static final int CONCURRENT = 0x00001000;
+         *  public static final int SUBSIZED = 0x00004000;
          */
 
-
+        Spliterator<String> stringSpliterator = new Spliterator();
 
     }
 
