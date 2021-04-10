3d1af359ebb6e3349307fca8c57c425d67bd5964
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-09 23:56:05 -0500

    Spliterator Characteristics

--- FP-08-Spliterator-Spliteraor-Characteristics/src/main/java/spliteratorcharacteristics/SpliteratorCharacteristics.java
+++ FP-08-Spliterator-Spliteraor-Characteristics/src/main/java/spliteratorcharacteristics/SpliteratorCharacteristics.java
@@ -15,7 +15,8 @@ public class SpliteratorCharacteristics {
          * Spliterator characteristics are used to define the state of the stream
          * int characteristics() method returns the set of characteristics of the spliterator and its elements
          *
-         *  public static final int ORDERED    = 0x00000010;
+         * Constants below are actual spliterator characteristics from the Spliterator interface
+         *  public static final int ORDERED    = 0x00000010; - value is a binary representation of a plain integer
          *  public static final int DISTINCT   = 0x00000001;
          *  public static final int SORTED     = 0x00000004;
          *  public static final int SIZED      = 0x00000040;
@@ -23,6 +24,7 @@ public class SpliteratorCharacteristics {
          *  public static final int IMMUTABLE  = 0x00000400;
          *  public static final int CONCURRENT = 0x00001000;
          *  public static final int SUBSIZED = 0x00004000;
+         *
          */
 
 
