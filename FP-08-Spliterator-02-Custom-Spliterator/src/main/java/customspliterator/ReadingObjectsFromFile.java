1cfa3b673da38402db1701736757871e222c55fd
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-11 10:55:24 -0500

    Custom Spliterator

--- FP-08-Spliterator-02-Custom-Spliterator/src/main/java/customspliterator/ReadingObjectsFromFile.java
+++ FP-08-Spliterator-02-Custom-Spliterator/src/main/java/customspliterator/ReadingObjectsFromFile.java
@@ -1,7 +1,21 @@
 package customspliterator;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ReadingObjectsFromFile {
     public static void main(String[] args) {
+
+        Path path = Paths.get("/Users/mykhailoselivanov/Documents/java-projects/java-functional-programming/FP-08-Spliterator-02-Custom-Spliterator/src/main/java/customspliterator/Books");
+
+        try {
+            Stream<String> lines = Files.lines(path);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
     }
 
