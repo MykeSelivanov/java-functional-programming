66086cbc17c8490436ffa59b88ce3e029dd687dd
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-17 14:15:12 -0500

    Collectors - Terminal Analogous Collectors

--- Fp-09-Collectors-Terminal-Analogous-Collectors/src/main/java/terminalanalogouscollectors/CollectorsInAction.java
+++ Fp-09-Collectors-Terminal-Analogous-Collectors/src/main/java/terminalanalogouscollectors/CollectorsInAction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class CollectorsInAction {
@@ -12,6 +13,9 @@ public class CollectorsInAction {
         Path path = Paths.get("/Users/mykhailoselivanov/Documents/java-projects/java-functional-programming/Fp-09-Collectors-Terminal-Analogous-Collectors/src/main/java/terminalanalogouscollectors/EmployeeData");
 
         try (Stream<String> lines = Files.lines(path)) {
+
+            // getting flatten stream on all the words present in the file
+            Stream<String> words = lines.flatMap(line -> Arrays.stream(line.split(",")));
 
         } catch (IOException e) {
             e.printStackTrace();
