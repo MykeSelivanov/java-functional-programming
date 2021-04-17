1794d3c417f33730fde9b1ca0bcb6164f1ef5bc1
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-17 14:11:14 -0500

    Collectors - Terminal Analogous Collectors

--- Fp-09-Collectors-Terminal-Analogous-Collectors/src/main/java/terminalanalogouscollectors/CollectorsInAction.java
+++ Fp-09-Collectors-Terminal-Analogous-Collectors/src/main/java/terminalanalogouscollectors/CollectorsInAction.java
@@ -1,4 +1,15 @@
 package terminalanalogouscollectors;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class CollectorsInAction {
+    public static void main(String[] args) {
+
+        Path path = Paths.get("/Users/mykhailoselivanov/Documents/java-projects/java-functional-programming/Fp-09-Collectors-Terminal-Analogous-Collectors/src/main/java/terminalanalogouscollectors/EmployeeData");
+        Stream<String>
+
+    }
+
 }
