48282944bf35fe70ee4661508678a4874e8a1ab1
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-01 20:54:13 -0500

    Collectors - Cascading Collectors

---
+++ FP-09-Collectors-02-Cascading-Collectors/src/main/java/cascadingcollectors/Employee.java
@@ -0,0 +1,45 @@
+package cascadingcollectors;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@ToString
+public class Employee implements Comparable<Employee> {
+
+    private int id;
+    private String name;
+    private char gender;
+    private Date dob;
+    private String city;
+    private String designation;
+    private Date joiningDate;
+    private double salary;
+
+    public Employee(int id, String name, char gender, Date dob, String city, String designation, Date joiningDate, double salary) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.dob = dob;
+        this.city = city;
+        this.designation = designation;
+        this.joiningDate = joiningDate;
+        this.salary = salary;
+    }
+
+    // implementing Employee comparator based on Employee ID
+    @Override
+    public int compareTo(Employee o) {
+        if (this.id < o.id) {
+            return -1;
+        } else if (this.id > o.id) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
