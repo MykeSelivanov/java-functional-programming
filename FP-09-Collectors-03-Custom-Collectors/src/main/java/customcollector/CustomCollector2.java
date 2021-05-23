9e9b812e3b8ed86289a9cccde0b2830dcb08aef9
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-22 21:35:08 -0500

    Collectors - Creating Custom Collectors

---
+++ FP-09-Collectors-03-Custom-Collectors/src/main/java/customcollector/CustomCollector2.java
@@ -0,0 +1,13 @@
+package customcollector;
+
+import java.util.List;
+
+public class CustomCollector2 {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(2,6,8,7,9,0,1,52,5,61,8,9,96,0,18,23);
+
+
+    }
+
+}
