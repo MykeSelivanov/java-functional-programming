8064f977acbfbef83d37e29821e56aeb77e48108
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-31 21:40:23 -0500

    Collectors - Creating Custom Collectors

--- FP-09-Collectors-03-Custom-Collectors/src/main/java/customcollector/CustomCollector2.java
+++ FP-09-Collectors-03-Custom-Collectors/src/main/java/customcollector/CustomCollector2.java
@@ -1,13 +1,22 @@
 package customcollector;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 
 public class CustomCollector2 {
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(2,6,8,7,9,0,1,52,5,61,8,9,96,0,18,23);
 
-
+        Collector toList = Collector.of(
+                () -> new ArrayList<>(),    // supplier
+                (list, currentElement) -> list.add(currentElement),     // accumulator - biconsumer
+                (list1, list2) -> {
+                    list1.addAll(list2);            // BiFunction
+                    return list1;
+                },
+                Collector.Characteristics.IDENTITY_FINISH);
 
     }
 
