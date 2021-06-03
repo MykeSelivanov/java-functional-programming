3358d1ba5927e96c415f460efb940c3ea0c876ca
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-02 22:24:29 -0500

    Collectors - Creating Custom Collectors

--- FP-09-Collectors-03-Custom-Collectors/src/main/java/customcollector/CustomCollector2.java
+++ FP-09-Collectors-03-Custom-Collectors/src/main/java/customcollector/CustomCollector2.java
@@ -1,23 +1,51 @@
 package customcollector;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class CustomCollector2 {
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(2,6,8,7,9,0,1,52,5,61,8,9,96,0,18,23);
 
-        Collector toList = Collector.of(
+        Collector<Integer, List<Integer>, List<Integer>> toListCollector = Collector.of(
                 () -> new ArrayList<>(),    // supplier
-                (list, currentElement) -> list.add(currentElement),     // accumulator - biconsumer
+                (list, currentElement) -> list.add(currentElement),     // accumulator - BiConsumer
                 (list1, list2) -> {
-                    list1.addAll(list2);            // BiFunction
+                    list1.addAll(list2);            // combiner - BiFunction
                     return list1;
                 },
                 Collector.Characteristics.IDENTITY_FINISH);
 
+        List<Integer> evens = numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .collect(toListCollector);
+        evens.forEach(System.out::println);
+
+        System.out.println("===============================");
+
+        // use the overloaded method of Collector.of which takes (supplier, accumulator, combiner, finisher and characteristics)
+        // finisher step will be used to sort the list
+        // finisher is most commonly used to cast the type of stream elements, but you can also use it for different purposes, like soring in the case below
+        Collector<Integer, List<Integer>, List<Integer>> toSortedListCollector = Collector.of(
+                () -> new ArrayList<>(),    // supplier
+                (list, currentElement) -> list.add(currentElement),     // accumulator - BiConsumer
+                (list1, list2) -> {
+                    list1.addAll(list2);            // combiner - BiFunction
+                    return list1;
+                },
+                (list) -> {
+                    Collections.sort(list);        // finisher
+                    return list;
+                },
+                Collector.Characteristics.UNORDERED);
+
+        List<Integer> sortedList = numbers.stream()
+                                        .collect(toSortedListCollector);
+        sortedList.forEach(System.out::println);
     }
 
 }
