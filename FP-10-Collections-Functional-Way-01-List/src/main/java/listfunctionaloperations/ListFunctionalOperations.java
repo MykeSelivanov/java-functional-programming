be8eefc49b2aed434b4298d85a8e956e1c98f9b9
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-05 20:19:44 -0500

    List - playing with Collections Functional way

--- FP-10-Collections-Functional-Way-01-List/src/main/java/listfunctionaloperations/ListFunctionalOperations.java
+++ FP-10-Collections-Functional-Way-01-List/src/main/java/listfunctionaloperations/ListFunctionalOperations.java
@@ -1,7 +1,19 @@
 package listfunctionaloperations;
 
+import java.util.Arrays;
+
 public class ListFunctionalOperations {
     public static void main(String[] args) {
+
+        List<Movie> movies = Arrays.asList(
+                new Movie("Spotlight",2015,"Hollywood"),
+                new Movie("Avengers: Infinity War",2018,"Hollywood"),
+                new Movie("Inception",2010,"Hollywood"),
+                new Movie("Forest Gump",1994,"Hollywood"),
+                new Movie("3 Idiots",2009,"Bollywood"),
+                new Movie("Beauty and the beast",2017,"Hollywood"),
+                new Movie("Slumdog Millionaire",2008,"Bollywood")
+        );
 
 
 
