b45a7b6d803b800083a3750076f5e98e664818f6
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-13 19:14:16 -0500

    Map - playing with Collections Functional way

---
+++ FP-10-Collections-Functional-Way-03-Map/src/main/java/mapfunctionaloperations/MapFunctionalOperations.java
@@ -0,0 +1,19 @@
+package mapfunctionaloperations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapFunctionalOperations {
+    public static void main(String[] args) {
+
+        Map<String, String> contacts = new HashMap<>();
+        contacts.put("1234567890", "John");
+        contacts.put("1112223334", "John");
+        contacts.put("4445556667", "Peter");
+        contacts.put("8899006743", "Jackie");
+        contacts.put("9944331235", "Nina");
+        contacts.put("0507643271", "Edgar");
+        contacts.put("3114567898", "Alice");
+
+    }
+}
