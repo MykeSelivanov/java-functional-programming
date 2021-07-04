284854efd18f824c45eee282c43b470d3ccb33a1
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-04 14:01:14 -0500

    Data Structures - Functional Way - Intro

---
+++ FP-11-Data-Structures-Functional-Way-01-List/src/main/java/intro/Intro.java
@@ -0,0 +1,23 @@
+package intro;
+
+public class Intro {
+
+    /**
+     * Functional Data Structure
+     * 1. Not imperative, implemented in functional way:
+     *      - Functional DS don't have any imperative features - there will be no updates or modifications, and no such
+     *      operations that will have side effects
+     *
+     * 2. Always Immutable and Persistent:
+     *      In java there are mutable data structures present in Collection API, if you look at the available methods,
+     *      many of them have VOID return type, which means that this method is mutating the state of passed object rather
+     *      creating a new object, therefore mutable collections are not thread safe.
+     *
+     * 3. Persistency
+     *      Any modification occurs in data structure -> have to preserve th previous state of itself
+     *      Any modification on a functional data structure does not change the existing version, it produces a new version for that
+     *
+     */
+
+
+}
