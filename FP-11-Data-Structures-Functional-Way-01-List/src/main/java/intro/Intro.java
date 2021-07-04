946f768f9bfc9d51a2e3b3bda7ce983e18e39b32
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-04 14:06:52 -0500

    Data Structures - Functional Way - Intro

--- FP-11-Data-Structures-Functional-Way-01-List/src/main/java/intro/Intro.java
+++ FP-11-Data-Structures-Functional-Way-01-List/src/main/java/intro/Intro.java
@@ -13,9 +13,20 @@ public class Intro {
      *      many of them have VOID return type, which means that this method is mutating the state of passed object rather
      *      creating a new object, therefore mutable collections are not thread safe.
      *
-     * 3. Persistency
+     * 3. Persistence
      *      Any modification occurs in data structure -> have to preserve th previous state of itself
      *      Any modification on a functional data structure does not change the existing version, it produces a new version for that
+     *
+     * 4. Methods of functional data structures are referentially transparent
+     *      Call a function once, catch the value, and later you can use it anywhere - simpler means, that for some given
+     *      input the output will always be the same
+     *
+     *      Mutable data structure - Need to guard access
+     *                               Put locks
+     *                               Take care of synchronization
+     *
+     *      Functional data structure - Secure
+     *                                  Immutable
      *
      */
 
