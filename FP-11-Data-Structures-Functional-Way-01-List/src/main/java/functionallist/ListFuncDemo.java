9eb87ca1e6e30445a8d17f8a67dbd31edfb8bc7b
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-17 14:45:10 -0500

    Data Structures - Functional Way - List

--- FP-11-Data-Structures-Functional-Way-01-List/src/main/java/functionallist/ListFuncDemo.java
+++ FP-11-Data-Structures-Functional-Way-01-List/src/main/java/functionallist/ListFuncDemo.java
@@ -3,7 +3,8 @@ package functionallist;
 public class ListFuncDemo {
     public static void main(String[] args) {
 
-
+        ListFunc<Integer> listFunc = ListFunc.list(3,7,9,11);
+        listFunc.forEach(System.out::println);
 
     }
 }
