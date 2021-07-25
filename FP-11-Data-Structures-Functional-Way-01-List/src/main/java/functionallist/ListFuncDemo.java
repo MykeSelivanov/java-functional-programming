1549ecff44536b4584e07824868a5c2da8a4a6fd
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-24 19:12:35 -0500

    Data Structures - Functional Way - List

--- FP-11-Data-Structures-Functional-Way-01-List/src/main/java/functionallist/ListFuncDemo.java
+++ FP-11-Data-Structures-Functional-Way-01-List/src/main/java/functionallist/ListFuncDemo.java
@@ -3,17 +3,23 @@ package functionallist;
 public class ListFuncDemo {
     public static void main(String[] args) {
 
-        ListFunc<Integer> listFunc = ListFunc.list(3,7,9,11);
+        ListFunc<Integer> listFunc = ListFunc.list(3,7,9,5,14,11);
         // for each
         listFunc.forEach(System.out::println);
+
         System.out.println("--------------------");
 
         // add element - will return a new list with old list elements + new element
         ListFunc<Integer> listFunc1 = listFunc.addElement(45);
         listFunc1.forEach(System.out::println);
 
+        System.out.println("--------------------");
 
+        // remove an element
+        ListFunc<Integer> listWithout5 = listFunc.removeElement(5);
+        listWithout5.forEach(System.out::println);
 
 
     }
 }
+
