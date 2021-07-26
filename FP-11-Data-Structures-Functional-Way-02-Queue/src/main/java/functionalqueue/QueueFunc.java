7b323c474650d63ac76cb1f48f61adb3083415db
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-25 21:14:35 -0500

    Data Structures - Functional Way - Queue

--- FP-11-Data-Structures-Functional-Way-02-Queue/src/main/java/functionalqueue/QueueFunc.java
+++ FP-11-Data-Structures-Functional-Way-02-Queue/src/main/java/functionalqueue/QueueFunc.java
@@ -1,4 +1,14 @@
 package functionalqueue;
 
 public final class QueueFunc<T> {
+
+    private final ListFunc<T> front;
+    private final ListFunc<T> rear;
+
+    public QueueFunc() {
+        this.front = ListFunc.list();
+        this.rear = ListFunc.list();
+    }
+
+
 }
