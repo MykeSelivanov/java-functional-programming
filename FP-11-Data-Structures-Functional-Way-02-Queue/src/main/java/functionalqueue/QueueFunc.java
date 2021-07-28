69ae85b9551af783bf6690e7ab3314730ff4df00
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-27 23:54:32 -0500

    Data Structures - Functional Way - Queue

--- FP-11-Data-Structures-Functional-Way-02-Queue/src/main/java/functionalqueue/QueueFunc.java
+++ FP-11-Data-Structures-Functional-Way-02-Queue/src/main/java/functionalqueue/QueueFunc.java
@@ -5,9 +5,20 @@ public final class QueueFunc<T> {
     private final ListFunc<T> front;
     private final ListFunc<T> rear;
 
-    public QueueFunc() {
+    private QueueFunc() {
         this.front = ListFunc.list();
         this.rear = ListFunc.list();
+    }
+
+    // Empty Queue
+    public static <T> QueueFunc<T> queue(){
+        return new QueueFunc<T>();
+    }
+
+    private QueueFunc(QueueFunc<T> queue, T element){
+        boolean frontIsEmpty = queue.front.isEmpty();
+        this.front = frontIsEmpty ? queue.front.addElement(element) : queue.front;
+        this.rear = frontIsEmpty ? queue.rear : queue.rear.addElement(element);
     }
 
 
