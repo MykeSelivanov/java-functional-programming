e7a937cf9777a90018eda872820dd61cd8221e96
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-29 23:30:49 -0500

    Data Structures - Functional Way - Queue

--- FP-11-Data-Structures-Functional-Way-02-Queue/src/main/java/functionalqueue/QueueFuncDemo.java
+++ FP-11-Data-Structures-Functional-Way-02-Queue/src/main/java/functionalqueue/QueueFuncDemo.java
@@ -3,5 +3,14 @@ package functionalqueue;
 public class QueueFuncDemo {
     public static void main(String[] args) {
 
+        QueueFunc<String> q = QueueFunc.queue();
+
+        QueueFunc<String> queueElements = q.enqueue("Hello").enqueue("World").enqueue("Test").enqueue("Functional").enqueue("Queue");
+        queueElements.forEach(System.out::println);
+
+        System.out.println("-----------------");
+
+        
+
     }
 }
