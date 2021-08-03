3a0e28c7768af7af9f031191c5258eee422ade50
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-02 23:52:55 -0500

    Data Structures - Functional Way - Tree

---
+++ FP-11-Data-Structures-Functional-Way-03-Tree/src/main/java/functionaltree/TreeFuncDemo.java
@@ -0,0 +1,11 @@
+package functionaltree;
+
+public class TreeFuncDemo {
+    public static void main(String[] args) {
+
+        TreeFunc<Integer> tree1 = TreeFunc.tree(23, 5, 74, 87, 10, 11, 3, 45, 22);
+        System.out.println(tree1);
+
+
+    }
+}
