a183ded5b6399860b9c24274a695c9581ab6ccac
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-07 15:22:34 -0500

    Data Structures - Functional Way - Map

--- FP-11-Data-Structures-Functional-Way-04-Map/src/main/java/functionalmap/Entry.java
+++ FP-11-Data-Structures-Functional-Way-04-Map/src/main/java/functionalmap/Entry.java
@@ -24,4 +24,19 @@ public class Entry {
         this.key = key;
     }
 
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Entry getNext() {
+        return next;
+    }
+
+    public void setNext(Entry next) {
+        this.next = next;
+    }
 }
