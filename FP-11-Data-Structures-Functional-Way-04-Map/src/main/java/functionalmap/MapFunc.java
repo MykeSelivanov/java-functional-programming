8a3aa3a065c1a14001f7cae8190509a81cdb04d4
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-07 15:27:33 -0500

    Data Structures - Functional Way - Map

--- FP-11-Data-Structures-Functional-Way-04-Map/src/main/java/functionalmap/MapFunc.java
+++ FP-11-Data-Structures-Functional-Way-04-Map/src/main/java/functionalmap/MapFunc.java
@@ -1,5 +1,20 @@
 package functionalmap;
 
-public class MapFunc {
+public class MapFunc<K,V> {
 
+    private Entry[] entries;
+    private int size;
+
+    public MapFunc(int size) {
+        this.size = size;
+        this.entries = new Entry[size];
+
+        for (int i = 0; i < size; i++){
+            this.entries[i] = new Entry();
+        }
+    }
+
+    public Integer getHash(K key) {
+        return key.hashCode() % size;
+    }
 }
