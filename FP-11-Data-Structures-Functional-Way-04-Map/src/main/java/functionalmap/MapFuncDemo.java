62f872ebe9f549bd7e008bfe61f6374a6a0c8374
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-12 20:27:39 -0500

    Data Structures - Functional Way - Map

--- FP-11-Data-Structures-Functional-Way-04-Map/src/main/java/functionalmap/MapFuncDemo.java
+++ FP-11-Data-Structures-Functional-Way-04-Map/src/main/java/functionalmap/MapFuncDemo.java
@@ -3,5 +3,14 @@ package functionalmap;
 public class MapFuncDemo {
     public static void main(String[] args) {
 
+        MapFunc<Integer, String> mapFunc = new MapFunc<>(5);
+        mapFunc.put(1, "Peter");
+        mapFunc.put(1, "James");
+        mapFunc.put(1, "Jamie");
+        mapFunc.put(1, "Olha");
+        mapFunc.put(1, "Greg");
+
+        System.out.println(mapFunc.toString());
+
     }
 }
